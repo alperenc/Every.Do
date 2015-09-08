c18b84a1312b895e057d4c62a74063cafa57a539
Alp Eren Can <alperenc@gmail.com>
2015-09-08 11:20:28 -0700

    Initial Commit

---
+++ Every.Do/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Every.Do
+//
+//  Created by Alp Eren Can on 08/09/15.
+//  Copyright © 2015 Alp Eren Can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Every.Do/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Every.Do
+//
+//  Created by Alp Eren Can on 08/09/15.
+//  Copyright © 2015 Alp Eren Can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
